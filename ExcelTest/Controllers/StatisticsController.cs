a49618cbf0548849128f143a3212a7016a078e62
zmartl <luca.marti@bluewin.ch>
2017-04-28 16:10:21 +0200

    add reports

--- ExcelTest/Controllers/StatisticsController.cs
+++ ExcelTest/Controllers/StatisticsController.cs
@@ -18,40 +18,24 @@ namespace ExcelTest.Controllers
     public class StatisticsController : Controller
     {
         private readonly ExcelTestContext db = new ExcelTestContext();
-        private readonly ExcelPackage p = new ExcelPackage();
-        private readonly Color _blue = Color.FromArgb(230, 43, 39);
+        private readonly Color _red = Color.FromArgb(230, 43, 39);
 
         // GET: Statistics
         public ActionResult Index()
         {            
             return View(db.Statistics.ToList());
-        }        
-
-        private Stream DownloadImage(string url)
-        {
-            try
-            {
-                WebRequest req = WebRequest.Create(url);
-                WebResponse response = req.GetResponse();
-                Stream stream = response.GetResponseStream();
-                return stream;
-            }
-            catch (Exception)
-            {
-                //TODO
-            }
-            return null;
-        }        
+        }
 
         /// <summary>
-        ///     Set the default Layout for one worksheet for Airport Zurich (Header and Footer)
+        ///     Set the default Layout for one worksheet (Header and Footer)
         /// </summary>
         /// <param name="worksheet">actual worksheet</param>
-        private void SetTemplate(ExcelWorksheet worksheet)
+        /// <param name="isPlanning"></param>
+        private void SetTemplate(ExcelWorksheet worksheet, bool isPlanning)
         {
             try
             {
-                var originalBitmap = new Bitmap("C:\\Development\\ExcelTest\\ExcelTest\\stadtuster.png");
+                var originalBitmap = new Bitmap(Server.MapPath("~/stadtuster.png"));
                 var image = new Bitmap(originalBitmap, 135, 50);
                 worksheet.HeaderFooter.OddHeader.InsertPicture(image, PictureAlignment.Left);
             }
@@ -59,29 +43,55 @@ namespace ExcelTest.Controllers
             {
                 //TODO
             }
-            worksheet.HeaderFooter.OddHeader.RightAlignedText = "Fahrzeugstatistik - Export";
+            if (isPlanning)
+            {
+                worksheet.HeaderFooter.OddHeader.RightAlignedText = "Fahrzeugplanung - Export";
+                worksheet.Column(1).Width = 23;
+                worksheet.Column(2).Width = 23;
+                worksheet.Column(3).Width = 23;
+                worksheet.Column(4).Width = 20;
+            }
+            else
+            {
+                worksheet.HeaderFooter.OddHeader.RightAlignedText = "Fahrzeugstatistik - Export";
+                worksheet.Column(1).Width = 27;
+                worksheet.Column(2).Width = 27;
+                worksheet.Column(3).Width = 35;
+
+            }
             worksheet.HeaderFooter.OddFooter.RightAlignedText = string.Format("{0} von {1}",
                 ExcelHeaderFooter.PageNumber, ExcelHeaderFooter.NumberOfPages);
-            worksheet.HeaderFooter.OddFooter.LeftAlignedText = ExcelHeaderFooter.FileName;
 
-            worksheet.PrinterSettings.Orientation = eOrientation.Landscape;
+            worksheet.PrinterSettings.Orientation = eOrientation.Portrait;
 
             // Change the sheet view to show it in page layout mode
             worksheet.View.PageLayoutView = true;
         }
 
-        private int GetCountPlanningsByState(string state)
+        public int GetCountPlannedPlanningsByState(IEnumerable<Planning> plannings, string state)
         {
-            var res = db.Plannings.Where(x => x.State.Name.Equals(state));
-            return res.Count();
+            var count =  plannings.Count(x => x.State.Name.Equals(state));
+            return count;
+        }
+
+        public IEnumerable<Planning> GetPlannedPlannings(DateTime starTime, DateTime endTime)
+        {
+            var plannedCars = db.Plannings.Where(
+                        x =>
+                            x.StartTime <= starTime && starTime <= x.EndTime ||
+                            x.StartTime <= endTime && endTime <= x.EndTime ||
+                            starTime <= x.StartTime && endTime >= x.EndTime).ToList();
+
+            return plannedCars;
         }
 
         /// <summary>
         ///     Create the Excel-File with all devices
         /// </summary>
-        public FileInfo CreateSheet()
+        public FileInfo CreateSingleCarSheet()
         {
-            Car car = new Car {Description = "BMW", Radio = "9801"};
+            bool isPlanning = false;
+            Car car = new Car {Description = "BMW", Radio = "9801", Id = 1};
             DateTime starttime = DateTime.Now;
             DateTime endtime = DateTime.Now.AddDays(2);
 
@@ -93,39 +103,39 @@ namespace ExcelTest.Controllers
             {
                 var worksheet = package.Workbook.Worksheets.Add("Fahrzeugstatistik");
 
-                SetTemplate(worksheet);
+                SetTemplate(worksheet, isPlanning);
 
-                var allEntities = db.Plannings.ToList();
+                var allEntities = GetPlannedPlannings(starttime, endtime).Where(x => x.Car.Id == car.Id);
 
-                var countDevices = allEntities.Count;
-
+                var countPlannings = allEntities.Count();
 
                 //Add the header informations
                 //First row
                 // COL / ROW
-                worksheet.Cells[1, 1].Style.Font.Size = 16;
-                worksheet.Cells[1, 1].Style.Font.Bold = true;
+
+                worksheet.Cells[1, 1].Style.Font.Size = 18;
+                worksheet.Cells[1, 1].Style.Font.Bold = true;                
                 worksheet.Cells[1, 1].Value = "Auswertung - " + car.Description + " " + car.Radio;
                 worksheet.Cells[2, 1].Style.Font.Size = 14;
                 worksheet.Cells[2, 1].Value = starttime.ToString("d") + " - " + endtime.ToString("d");
                 worksheet.Cells[5, 1].Value = "Erstelldatum:";
                 worksheet.Cells[5, 2].Value = DateTime.Now.ToString("dd.MM.yyyy H:mm");
                 worksheet.Cells[6, 1].Value = "Ersteller:";
-                worksheet.Cells[6, 2].Value = "Marti, Luca";
+                worksheet.Cells[6, 2].Value = "Marti, Luca";                
 
                 //Second row
                 worksheet.Cells[8, 1].Style.Font.Bold = true;
                 worksheet.Cells[8, 1].Value = "Anzahl Einträge:";
                 worksheet.Cells[8, 2].Style.Font.Bold = true;
                 worksheet.Cells[8, 2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                worksheet.Cells[8, 2].Value = countDevices;
+                worksheet.Cells[8, 2].Value = countPlannings;
 
                 var col = 9;
                 foreach (var state in db.States)
                 {
                     worksheet.Cells[col, 1].Value = state.Name + ":";
                     worksheet.Cells[col, 2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                    worksheet.Cells[col, 2].Value = GetCountPlanningsByState(state.Name);
+                    worksheet.Cells[col, 2].Value = GetCountPlannedPlanningsByState(allEntities, state.Name);
                     col++;
                 }
 
@@ -137,12 +147,12 @@ namespace ExcelTest.Controllers
                 worksheet.Cells[tableHeader, 2].Value = "Endzeit";
                 worksheet.Cells[tableHeader, 3].Value = "Status";
 
-                foreach (var device in allEntities)
+                foreach (var planning in allEntities)
                 {
                     col++;
-                    worksheet.Cells[col, 1].Value = device.StartTime.ToString("d") + " " + device.StartTime.ToString("t"); 
-                    worksheet.Cells[col, 2].Value = device.EndTime.ToString("d") + " " + device.EndTime.ToString("t");
-                    worksheet.Cells[col, 3].Value = device.State.Name;                    
+                    worksheet.Cells[col, 1].Value = planning.StartTime.ToString("d") + " " + planning.StartTime.ToString("t"); 
+                    worksheet.Cells[col, 2].Value = planning.EndTime.ToString("d") + " " + planning.EndTime.ToString("t");
+                    worksheet.Cells[col, 3].Value = planning.State.Name;                    
                 }
 
                 //Format the Header
@@ -161,23 +171,112 @@ namespace ExcelTest.Controllers
                 {
                     range.Style.Font.Bold = true;
                     range.Style.Fill.PatternType = ExcelFillStyle.Solid;
-                    range.Style.Fill.BackgroundColor.SetColor(_blue);
+                    range.Style.Fill.BackgroundColor.SetColor(_red);
                     range.Style.Font.Color.SetColor(Color.White);
-                    range.AutoFitColumns();
                     range.AutoFilter = true;
-                    range.AutoFitColumns(1);
-                    range.AutoFitColumns(2);
-                    range.AutoFitColumns(3);
                 }
 
+
                 //Set property values
-                package.Workbook.Properties.Subject = "Gerätelebenslauftool";
-                package.Workbook.Properties.Title = "Geräteinventarliste";
+                package.Workbook.Properties.Subject = "Fahrzeugstatistik";
+                package.Workbook.Properties.Title = "Fahrzeugstatistik";
 
                 //Set extended property values
-                package.Workbook.Properties.Company = "Flughafen Zürich AG";
+                package.Workbook.Properties.Company = "Verwaltungspolizei Stadtpolizei Uster";
 
-                //package.Save();
+                this.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                this.Response.AddHeader(
+                          "content-disposition",
+                          string.Format("attachment;  filename={0}", filename));
+                this.Response.BinaryWrite(package.GetAsByteArray());
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        ///     Create the Excel-File with all devices
+        /// </summary>
+        public FileInfo CreateOverviewSheet()
+        {
+            bool isPlanning = true;
+
+            DateTime starttime = DateTime.Now;
+            DateTime endtime = DateTime.Now.AddDays(2);
+
+            var filename = "Planung_" + DateTime.Now.ToString("d") + ".xlsx";
+
+            var response = new FileInfo(filename);
+
+            using (var package = new ExcelPackage(response))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Fahrzeugplanung");
+
+                SetTemplate(worksheet, isPlanning);
+
+                var allEntities = GetPlannedPlannings(starttime, endtime).OrderBy(x => x.StartTime);
+
+                //Add the header informations
+                //First row
+                // COL / ROW
+                worksheet.Cells[1, 1].Style.Font.Size = 18;
+                worksheet.Cells[1, 1].Style.Font.Bold = true;
+                worksheet.Cells[1, 1].Value = "Fahrzeugplanung";
+                worksheet.Cells[2, 1].Style.Font.Size = 14;
+                worksheet.Cells[2, 1].Value = starttime.ToString("d") + " - " + endtime.ToString("d");
+                worksheet.Cells[5, 1].Value = "Erstelldatum:";
+                worksheet.Cells[5, 2].Value = DateTime.Now.ToString("dd.MM.yyyy H:mm");
+                worksheet.Cells[6, 1].Value = "Ersteller:";
+                worksheet.Cells[6, 2].Value = "Marti, Luca";
+
+                var col = 7;
+
+                col = col + 2;
+                var tableHeader = col;
+
+                //Add the content-headers
+                worksheet.Cells[tableHeader, 1].Value = "Startzeit";
+                worksheet.Cells[tableHeader, 2].Value = "Endzeit";
+                worksheet.Cells[tableHeader, 3].Value = "Fahrzeug";
+                worksheet.Cells[tableHeader, 4].Value = "Status";
+
+                foreach (var planning in allEntities)
+                {
+                    col++;
+                    worksheet.Cells[col, 1].Value = planning.StartTime.ToString("d") + " " + planning.StartTime.ToString("t");
+                    worksheet.Cells[col, 2].Value = planning.EndTime.ToString("d") + " " + planning.EndTime.ToString("t");
+                    worksheet.Cells[col, 3].Value = planning.Car.Description + " - " + planning.Car.Radio;
+                    worksheet.Cells[col, 4].Value = planning.State.Name;
+                }
+
+                //Format the Header
+                using (var range = worksheet.Cells[4, 1, 4, 4])
+                {
+                    range.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+                }
+                var borderEnd = tableHeader - 2;
+                using (var range = worksheet.Cells[borderEnd, 1, borderEnd, 4])
+                {
+                    range.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+                }
+
+                // Format the List-Header 
+                using (var range = worksheet.Cells[tableHeader, 1, tableHeader, 4])
+                {
+                    range.Style.Font.Bold = true;
+                    range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    range.Style.Fill.BackgroundColor.SetColor(_red);
+                    range.Style.Font.Color.SetColor(Color.White);
+                    range.AutoFilter = true;
+                }
+
+
+                //Set property values
+                package.Workbook.Properties.Subject = "Fahrzeugplanung";
+                package.Workbook.Properties.Title = "Fahrzeugplanung";
+
+                //Set extended property values
+                package.Workbook.Properties.Company = "Verwaltungspolizei Stadtpolizei Uster";
 
                 this.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
                 this.Response.AddHeader(
